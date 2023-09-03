d087ff360c050c6536d726c202aed63743c7089b
zzq <zzqzzqzqzzq@163.com>
2023-09-03 15:06:45 +0800

    Initial Commit

---
+++ xcodetrying/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  xcodetrying
+//
+//  Created by 张智清 on 2023/9/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
